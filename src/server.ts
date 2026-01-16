ac2a737bba9044a0a90745c623202da5703bf9d4
joseh33 <josehsejoh33@gmail.com>
2026-01-17 01:26:18 +0300

    Initial commit: backend

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 const corsOptions = {
   origin: [
     'https://pesapalrdbms.netlify.app',
-    'https://pesapal-frontend-rdbms-hzy9.vercel.app',
+    'https://pesapal-frontend-rdbms-hzy9-gdhqujcot-sejoh2s-projects.vercel.app',
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:5173',
